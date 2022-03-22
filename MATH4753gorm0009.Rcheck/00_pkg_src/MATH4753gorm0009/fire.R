b2ee6845d70bb84999b441f72ac61bb04974d18c
Christine Gormley <christine.m.gormley-1@ou.edu>
2022-03-22 16:26:16 -0500

    "giga push"

---
+++ MATH4753gorm0009.Rcheck/00_pkg_src/MATH4753gorm0009/fire.R
@@ -0,0 +1,12 @@
+#' Distance and damage caused by fires.
+#'
+#' A dataset describing the distance and damage from 15 fires.
+#'
+#' @format A data frame with 3 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{The distance of which the fire burned.}
+#'   \item{DAMAGE}{ The damage the fire caused}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"fire"
